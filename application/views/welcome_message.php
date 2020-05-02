187c3a3c21eeeec3c04026d459d2e134b8bc12b0
jahangirpial <jahangiralompial@gmail.com>
2020-05-02 15:08:37 +0600

    Welcome Message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,12 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to ciAuth</title>
+	<title>Welcome</title>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome!</h1>
 </div>
 
 </body>
